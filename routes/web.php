3ca71fd7cc746de5ec06a88badb16f47c1e4fdf7
mhmdrazn <razan4944@gmail.com>
2025-11-04 00:31:28 +0700

    supabase

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-db', function () {
+    return DB::table('app_user')->get();
+});
+
+use Illuminate\Support\Facades\DB;
+
+Route::get('/peminjaman', function () {
+    $peminjaman = DB::table('peminjaman')
+        ->join('app_user', 'peminjaman.userid', '=', 'app_user.userid')
+        ->select('peminjaman.peminjamanid', 'app_user.nama', 'peminjaman.keterangan', 'peminjaman.dokumen')
+        ->get();
+
+    return view('peminjaman', ['peminjaman' => $peminjaman]);
+});
